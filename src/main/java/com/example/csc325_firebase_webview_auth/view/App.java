b31a88480392744adbbb142b62aae3d5435ca82d
AaronScott2025 <147019715+AaronScott2025@users.noreply.github.com>
2024-10-08 23:48:24 -0400

    GUI update

--- src/main/java/com/example/csc325_firebase_webview_auth/view/App.java
+++ src/main/java/com/example/csc325_firebase_webview_auth/view/App.java
@@ -5,6 +5,8 @@ import com.example.csc325_firebase_webview_auth.model.FirestoreContext;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.auth.FirebaseAuth;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -23,9 +25,18 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        fstore = contxtFirebase.firebase();
+        scene = new Scene(loadFXML("/files/Splash.fxml"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        try {
+            fstore = contxtFirebase.firebase();
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            System.out.println("Error, could not connect to database");
+        }
         fauth = FirebaseAuth.getInstance();
         scene = new Scene(loadFXML("/files/AccessFBView.fxml"));
+        scene.getStylesheets().add("/files/style.css");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
---
+++ src/main/java/com/example/csc325_firebase_webview_auth/view/splashController.java
@@ -0,0 +1,19 @@
+package com.example.csc325_firebase_webview_auth.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class splashController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private ImageView leCow;
+
+    @FXML
+    private Label loadinglbl;
+
+}
